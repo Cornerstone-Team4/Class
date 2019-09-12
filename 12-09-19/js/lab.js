650dc8fe66d4984b57eaa39682655300e3d323bc
friikjones <d.mullerjones@gmail.com>
2019-09-12 11:56:06 -0700

    12-09-19 12:00

---
+++ 12-09-19/js/lab.js
@@ -0,0 +1,98 @@
+//------------------Calc------------------------------
+var firstNumberCalc;
+var secondNumberCalc;
+var outputCalc;
+
+function clickMultiply(){
+    firstNumberCalc = document.getElementById("firstNumberCalc").value;
+    secondNumberCalc = document.getElementById("secondNumberCalc").value;
+    outputCalc = parseInt(firstNumberCalc * secondNumberCalc);
+    document.getElementById("outputCalc").innerHTML = outputCalc;
+}
+
+function clickDivide(){
+    firstNumberCalc = document.getElementById("firstNumberCalc").value;
+    secondNumberCalc = document.getElementById("secondNumberCalc").value;
+    outputCalc = parseInt(firstNumberCalc / secondNumberCalc);
+    document.getElementById("outputCalc").innerHTML = outputCalc;
+}
+
+//----------------Conv------------------------------------
+var poundConv;
+var kgConv;
+var outputPoundConv;
+var outputKgConv
+
+
+function weightConverterKg(inputKg){
+    outputPoundConv = inputKg * (2.2);
+    document.getElementById("outputKgConv").innerHTML = outputPoundConv;
+}
+
+function weightConverterPounds(inputPounds){
+    outputKgConv = inputPounds / (2.2);
+    document.getElementById("outputPoundsConv").innerHTML = outputKgConv;
+}
+
+//--------------Array--------------------
+
+function sortPrompt(){
+    var sortInput;
+    var sortArray = [];
+    sortInput = document.getElementById("inputArray").innerHTML;
+    sortArray = sortInput.split(",");
+    document.getElementById("outputArray").innerHTML = sortArray.sort().toString();
+}
+
+function reversePrompt(){
+    var sortInput;
+    var sortArray = [];
+    sortInput = document.getElementById("inputArray").innerHTML;
+    sortArray = sortInput.split(",");
+    document.getElementById("outputArray").innerHTML = sortArray.sort().reverse().toString();
+}
+
+
+
+//------------Table--------------------
+
+var tableNumber;
+var outputTable;
+
+function tablePrompt()
+{
+    tableNumber = parseInt(prompt("Tell me a number from 1 to 10"));
+    generateTable();
+    document.getElementById("outputTable").innerHTML = outputTable;
+}
+
+function generateTable(){
+    var multiplier;
+    outputTable = "";
+    for (multiplier=1; multiplier <= 10; multiplier++) {
+        outputTable += tableNumber + "x" + multiplier + " = " + tableNumber*multiplier + "<br>";
+    }
+}
+
+
+//------------Factorial---------------
+
+var facNumber;
+var outputFac;
+
+function facPrompt()
+{
+    facNumber = parseInt(prompt("Tell me a number, I'll tell you the factorial"));
+    outputFac = factorialize(facNumber);
+    document.getElementById("outputFac").innerHTML = outputFac;
+}
+
+function factorialize(num) 
+{
+    if (num === 0 || num === 1)
+      return 1;
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
+}
